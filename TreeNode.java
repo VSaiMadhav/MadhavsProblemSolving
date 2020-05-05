a3129b106745cf82c441de2ad2a04800c16a6bbb
VSaiMadhav <59437976+VSaiMadhav@users.noreply.github.com>
2020-05-05 10:49:19 +0530

    Add files via upload

---
+++ TreeNode.java
@@ -0,0 +1,16 @@
+package com.alg.myprac.binarytree;
+
+public class TreeNode {
+	TreeNode left;
+	TreeNode right;
+	int data;
+	
+	public TreeNode() {
+		
+	}
+	
+	public TreeNode(int data) {
+		this.data = data;
+	}
+
+}
